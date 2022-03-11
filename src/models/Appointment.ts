83317d8a5fd525276971ff6a695fcb3d63191dc3
Daniel Vidal <danielsv465@hotmail.com>
2022-03-11 10:22:09 -0300

    Create services and integrate postgresql

---
+++ src/models/Appointment.ts
@@ -0,0 +1,16 @@
+/* eslint-disable indent */
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider: string;
+
+  @Column('timestamp with time zone')
+  date: Date;
+}
+
+export default Appointment;
