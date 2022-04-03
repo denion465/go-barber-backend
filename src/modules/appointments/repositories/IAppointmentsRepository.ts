f3195a03dbe3acfcb27ae44c327a680f3ee4f748
Daniel Vidal <danielsv465@hotmail.com>
2022-04-03 11:23:55 -0300

    List available times

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,9 +1,13 @@
 import ICreateAppointmentDTO from '../dtos/ICreateAppoitmentDTO';
 import Appointment from '../infra/typeorm/entities/Appointment';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 interface IAppoitmenstRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(data: IFindAllInMonthFromProviderDTO): Promise<Appointment[]>
+  findAllInDayFromProvider(data: IFindAllInDayFromProviderDTO): Promise<Appointment[]>
 }
 
 export default IAppoitmenstRepository;
