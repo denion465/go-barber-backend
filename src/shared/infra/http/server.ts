a7dbdb33326ac2e6b7c070ea99d5a632fd34f0cc
Daniel Vidal <danielsv465@hotmail.com>
2022-04-09 11:36:48 -0300

    Rate limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,12 +1,13 @@
-import express, { NextFunction, Request, Response } from 'express';
-
 import 'reflect-metadata';
 import 'dotenv/config';
+
+import express, { NextFunction, Request, Response } from 'express';
 
 import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
 
+import rateLimiter from '@shared/middlewares/rateLimiter';
 import routes from './routes';
 import uploadConfig from '@config/upload';
 import '@shared/infra/typeorm';
@@ -14,6 +15,8 @@ import AppError from '@shared/errors/AppError';
 import '@shared/container';
 
 const app = express();
+
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
