0c5fd7ee071cf064487e739ad28be904f7f6b589
Daniel Vidal <danielsv465@hotmail.com>
2022-04-10 11:33:13 -0300

    GET with query params

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -6,7 +6,7 @@ import ListProviderAppoitmentsService from '@modules/appointments/services/ListP
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const listProviderAppoitments = container.resolve(
       ListProviderAppoitmentsService
@@ -14,9 +14,9 @@ export default class ProviderAppointmentsController {
 
     const appointments = await listProviderAppoitments.execute({
       provider_id,
-      day,
-      month,
-      year
+      day: Number(day),
+      month: Number(month),
+      year: Number(year)
     });
 
     return response.json(appointments);
